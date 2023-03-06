9f0a5760abb0bb1b1bb2243bfe72dd0413b4189b
Julio Cesar <126982253+juliogo91@users.noreply.github.com>
2023-03-05 23:23:48 -0300

    Add files via upload
    Programa de Índice de Massa Corpórea

---
+++ Imc.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ImcMetodo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Imc {
+
+    //ATRIBUTOS
+    private int peso = 0;
+     private float altura = 0;
+
+    public int getPeso() {
+        return peso;
+    }
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;                         //FORMATAR CÓDIGO alt + shift + f
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+     //MÉTODO
+    public float calcImc() {
+        return peso / (altura * altura);
+    }
+}
